68b89b659e0e2bf0fac505552da40ff7072c8389
Matthew Mueller <mattmueller@outlook.com>
2019-02-22 14:49:38 -0500

    Predefine a Nova login route.

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use STS\SocialiteAuth\Http\Controllers\SocialiteController;
 
 Route::get('socialite-auth/start', SocialiteController::class . '@redirectToProvider')->middleware(config('socialite-auth.middleware'))->name('oauth.start');
 Route::get('socialite-auth/login', SocialiteController::class . '@redirectToProvider')->middleware(config('socialite-auth.middleware'))->name('login');
+Route::get('socialite-auth/nova-login', SocialiteController::class . '@redirectToProvider')->middleware(config('socialite-auth.middleware'))->name('nova.login');
 Route::get('socialite-auth/callback', SocialiteController::class . '@handleProviderCallback')->middleware(config('socialite-auth.middleware'))->name('oauth.callback');
