5a6dd1c0cd1b49c5ebf8c1e2a7240161a6d28b3d
Joseph <joseph@stechstudio.com>
2020-11-18 17:31:53 -0500

    fix controller

--- src/Http/Controllers/SocialiteController.php
+++ src/Http/Controllers/SocialiteController.php
@@ -2,20 +2,13 @@
 
 namespace STS\SocialiteAuth\Http\Controllers;
 
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Routing\Controller;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Laravel\Socialite\Facades\Socialite;
 
-class SocialiteController extends Controller
+class SocialiteController
 {
-    use AuthenticatesUsers, ValidatesRequests;
-
     /**
      * Redirect the user to the OAuth Provider.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function redirectToProvider()
     {
@@ -27,8 +20,6 @@ class SocialiteController extends Controller
      * database by looking up their provider_id in the database.
      * If the user exists, log them in. Otherwise, create a new user then log them in. After that
      * redirect them to the authenticated users homepage.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function handleProviderCallback()
     {
